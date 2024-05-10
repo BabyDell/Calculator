c95a6479a3e96d892c2c9b92d77b498b446be1ee
BabyDell <A.Java.Coder7@gmail.com>
2024-05-10 12:50:45 -0700

    main functions completed, edge cases still needed

--- src/app/Components/DigitButton.tsx
+++ src/app/Components/DigitButton.tsx
@@ -4,11 +4,7 @@ import { ACTIONS } from "./Interface";
 export default function DigitButton({ digit = "", spanTwo = false, dispatch }) {
   return (
     <button
-      className={
-        spanTwo
-          ? "col-span-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded h-24 w-12/12"
-          : "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded h-24 w-12/12"
-      }
+      className={"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded h-24 w-12/12"}
       onClick={() => dispatch({type: ACTIONS.ADD_DIGIT, payload: {digit}})}
     >
       {digit}
--- src/app/Components/Interface.tsx
+++ src/app/Components/Interface.tsx
@@ -14,12 +14,89 @@ export const ACTIONS = {
 function reducer(state, { type, payload }) {
   switch (type) {
     case ACTIONS.ADD_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          currentOperand: payload.digit,
+          overwrite: false
+        }
+      }
+      if (payload.digit === "0" && state.currentOperand === "0") return state;
+      if (payload.digit === "." && state.currentOperand.includes("."))
+        return state;
       return {
         //returns new state object
         ...state,
         currentOperand: `${state.currentOperand || ""}${payload.digit}`,
       };
+    case ACTIONS.CHOOSE_OPERATION:
+      if (state.currentOperand == null && state.previousOperand == null) {
+        return state;
+      } 
+
+      if (state.currentOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation,
+        }
+      }
+
+      if (state.previousOperand == null) {
+
+        return {
+          ...state,
+          operation: payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand: null,
+        };
+      }
+
+      
+
+      return {
+        ...state,
+        previousOperand: evalute(state),
+        operation: payload.operation,
+        currentOperand: null,
+      };
+
+    case ACTIONS.CLEAR:
+      return {};
+    case ACTIONS.EVALUATE:
+      if ( state.previousOperand == null || state.currentOperand == null || state.currentOperand == null) return state;
+
+      return {
+        ...state,
+        overwrite: true,
+        previousOperand: null,
+        operation: null,
+        currentOperand: evalute(state),
+      }
   }
+
+}
+
+function evalute({ currentOperand, previousOperand, operation }) {
+  const prev = parseFloat(previousOperand);
+  const curr = parseFloat(currentOperand);
+  if (isNaN(prev) || isNaN(curr)) return "";
+  let computation = 0;
+  switch (operation) {
+    case "+":
+      computation = previousOperand + currentOperand;
+      break;
+    case "-":
+      computation = previousOperand - currentOperand;
+      break;
+    case "+":
+      computation = previousOperand / currentOperand;
+      break;
+    case "x":
+      computation = previousOperand * currentOperand;
+      break;
+  }
+
+  return computation.toString();
 }
 
 export default function Interface() {
@@ -38,13 +115,20 @@ export default function Interface() {
           <div className="currOutput">{currentOperand}</div>
         </div>
         <div className="grid grid-cols-4 gap-3">
-          <OperationButton operation="AC" dispatch={dispatch} spanTwo={true} />
+          <button
+            className={
+              "col-span-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded h-24 w-12/12"
+            }
+            onClick={() => dispatch({ type: ACTIONS.CLEAR, payload: {} })}
+          >
+            AC
+          </button>
           <OperationButton operation="DEL" dispatch={dispatch} />
           <OperationButton operation="÷" dispatch={dispatch} />
           <DigitButton digit="7" dispatch={dispatch} />
           <DigitButton digit="8" dispatch={dispatch} />
           <DigitButton digit="9" dispatch={dispatch} />
-          <OperationButton operation="X" dispatch={dispatch} />
+          <OperationButton operation="x" dispatch={dispatch} />
           <DigitButton digit="4" dispatch={dispatch} />
           <DigitButton digit="5" dispatch={dispatch} />
           <DigitButton digit="6" dispatch={dispatch} />
@@ -55,7 +139,14 @@ export default function Interface() {
           <OperationButton operation="+" dispatch={dispatch} />
           <DigitButton digit="0" dispatch={dispatch} />
           <DigitButton digit="." dispatch={dispatch} />
-          <OperationButton operation="=" dispatch={dispatch} spanTwo={true} />
+          <button
+            className={
+              "col-span-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded h-24 w-12/12"
+            }
+            onClick={() => dispatch({ type: ACTIONS.EVALUATE})}
+          >
+            =
+          </button>
         </div>
       </div>
     </div>
